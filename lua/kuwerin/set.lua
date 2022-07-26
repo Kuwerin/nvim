6e89b0f8bbffb4c6789e608eef0a986df6b16533
Simon Kuwerin <kuwerin@gmail.com>
2022-07-26 17:29:45 +0300

    Initial

---
+++ lua/kuwerin/set.lua
@@ -0,0 +1,21 @@
+-- vim.opt.mouse=a  --- disable mouse
+vim.opt.encoding = 'utf-8'
+vim.opt.number = true
+vim.opt.noswapfile = true
+vim.opt.scrolloff = 7
+vim.opt.background = 'dark'
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.fileformat = 'unix'
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.diffopt = 'vertical'
+vim.opt.wrap = true 
+-- vim.opt.fo-=tc
+vim.opt.signcolumn='yes'
+vim.opt.wildcharm='<C-z>'
+
+vim.cmd(filetype indent on)      " load filetype-specific indent files
