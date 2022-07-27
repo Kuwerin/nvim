4cd5b6fce1fa598b2298b5f7899b132bb4b0ff2c
Simon Kuwerin <kuwerin@gmail.com>
2022-07-27 13:44:28 +0300

    Add the list of enabled colorscheme

--- after/plugin/visual/colorscheme.lua
+++ after/plugin/visual/colorscheme.lua
@@ -1,2 +1,10 @@
+-- Coloscheme setup
+-- Available schemes:
+-- gruvbox
+-- gruvbox-material
+-- nord
+-- OceanicNext
+-- OceanicNextLight
+-- everforest
 vim.opt.background = "dark"
 vim.cmd("colorscheme gruvbox-material")
