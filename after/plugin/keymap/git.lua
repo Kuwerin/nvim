66b37ae98530a497257e30161e92ec69a8a3eaa7
Simon Kuwerin <kuwerin@gmail.com>
2022-07-27 13:38:10 +0300

    Update mapping

--- after/plugin/keymap/git.lua
+++ after/plugin/keymap/git.lua
@@ -7,7 +7,7 @@ local silent = { silent = true }
 nnoremap("<leader>gs", ":Git status<CR>", silent)
 nnoremap("<leader>gc", ":Git commit<CR>", silent)
 nnoremap("<leader>ga", ":Git add ")
-nnoremap("<leader>gA", ":Git add %", silent)
+nnoremap("<leader>gA", ":Git add %<CR>", silent)
 nnoremap("<leader>gp", ":Git push<CR> ", silent)
 nnoremap("<leader>gCo", ":Git checkout ")
 nnoremap("<leader>gd", ":Gdiffsplit<CR>", silent)
